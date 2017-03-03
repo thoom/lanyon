67e12182424c5ceb7c39b7a27c3086637519f8cc
Zach Peacock <zach.peacock@digicert.com>
2017-03-03 13:33:15 -0700

    Updated script to only update ssh config if pulling from git.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-# Disable Strict Host checking for non interactive git clones
-mkdir -p -m 0700 /root/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-
-if [ ! -z "$SSH_KEY" ]; then
- echo $SSH_KEY > /root/.ssh/id_rsa.base64
- base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
- chmod 600 /root/.ssh/id_rsa
-fi
-
 cd /var/www
 
 # Dont pull code down if the .git folder exists
 if [ ! -d ".git" ]; then
+
+  # Disable Strict Host checking for non interactive git clones
+  mkdir -p -m 0700 /root/.ssh
+  echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
+
+ if [ ! -z "$SSH_KEY" ]; then
+  echo $SSH_KEY > /root/.ssh/id_rsa.base64
+  base64 -d /root/.ssh/id_rsa.base64 > /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
+ fi
+
  # Pull down code from git for our site!
  if [ ! -z "$GIT_REPO" ]; then
    # Remove the test index file
